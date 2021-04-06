0d78fb17a03b9a18e175a167623f83b4e8155350
ayodele olufemi <ayodele.olufemi@mnsu.edu>
2021-04-06 07:07:44 -0500

    error at the point

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,52 +1,56 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import PropTypes from "prop-types";
+import GithubContext from "../../context/github/githubContext";
 
-const Search = ({ searchUsers, showClear, clearUsers, setAlert }) => {
-  const [text, setText] = useState("");
+const Search = ({ showClear, clearUsers, setAlert }) => {
+	const githubContext = useContext(GithubContext);
+	const [text, setText] = useState("");
 
-  const onChange = (e) => {
-    setText(e.target.value);
-  };
+	const onChange = (e) => {
+		setText(e.target.value);
+	};
 
-  const onSubmit = (e) => {
-    e.preventDefault();
-    if (text === "") {
-      setAlert("Please enter something to search", "light");
-    } else {
-      searchUsers(text);
-      setText("");
-    }
-  };
-  return (
-    <div>
-      <form onSubmit={onSubmit} className="form">
-        <input
-          type="text"
-          name="text"
-          placeholder="Search Users"
-          value={text}
-          onChange={onChange}
-        />
-        <input
-          type="submit"
-          value="Search"
-          className="btn btn-dark btn-block"
-        />
-      </form>
-      {showClear && (
-        <button className="btn btn-light btn-block" onClick={clearUsers}>
-          Clear
-        </button>
-      )}
-    </div>
-  );
+	const onSubmit = (e) => {
+		e.preventDefault();
+		if (text === "") {
+			setAlert("Please enter something to search", "light");
+		} else {
+			githubContext.searchUsers(text);
+			setText("");
+		}
+	};
+	return (
+		<div>
+			<form onSubmit={onSubmit} className="form">
+				<input
+					type="text"
+					name="text"
+					placeholder="Search Users"
+					value={text}
+					onChange={onChange}
+				/>
+				<input
+					type="submit"
+					value="Search"
+					className="btn btn-dark btn-block"
+				/>
+			</form>
+			{showClear && (
+				<button
+					className="btn btn-light btn-block"
+					onClick={clearUsers}
+				>
+					Clear
+				</button>
+			)}
+		</div>
+	);
 };
 
 Search.propTypes = {
-  searchUsers: PropTypes.func.isRequired,
-  clearUsers: PropTypes.func.isRequired,
-  showClear: PropTypes.bool.isRequired,
-  setAlert: PropTypes.func.isRequired,
+	clearUsers: PropTypes.func.isRequired,
+	showClear: PropTypes.bool.isRequired,
+	setAlert: PropTypes.func.isRequired,
 };
 
 export default Search;
